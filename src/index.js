7dc97ba154238fc0a5b4a9829d3b5050d8217a5c
Pablo Misael Peloc <pmpeloc@gmail.com>
2020-08-31 20:46:18 -0300

    created list category

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { GifExpertApp } from './components/GifExpertApp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />,
+    <GifExpertApp />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// crear en src el componente GifExpertApp
+// <h2>Gif Expert App</h2>
+// <hr />
