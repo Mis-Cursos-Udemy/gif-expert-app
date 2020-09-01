2ce44ac2861f3886b328b859078a51ee6a8a7af3
Pablo Misael Peloc <pmpeloc@gmail.com>
2020-08-31 21:13:43 -0300

    finish list categories

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories(cats => [...cats, 'HunterXHunter']);
+    }
+
     return (
-        <div>
+        <>
             <h2>GifExpertApp</h2>
             <hr />
-        </div>
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                    return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
+        </>
     )
 }
 
