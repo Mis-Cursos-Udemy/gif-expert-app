1348502fa07ba82e682466ca2c2c2c762a439ced
Pablo Misael Peloc <pmpeloc@gmail.com>
2020-09-16 18:03:01 -0300

    docs init

---
+++ docs/precache-manifest.46d979ed399c96599f0a787d6f1b8deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14bb5d2c5ad963e3aafe5cd1e55e6ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2208057697127cb1f796",
+    "url": "/static/css/main.7590743c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2208057697127cb1f796",
+    "url": "/static/js/main.c1cad6d5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
